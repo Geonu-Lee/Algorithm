711b5448018ec9d794841abce5531ba392a717ec
Geonu.Lee <99609743+Geonu-Lee@users.noreply.github.com>
2024-03-07 00:08:17 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -0,0 +1,14 @@
+# def solution(n):
+#     answer = 0
+#     while n > 0:
+#         if n % 2 == 0:
+#             n //= 2
+#         else:
+#             n -= 1
+#             answer += 1
+
+#     return answer
+
+def solution(n):
+    
+    return bin(n).count("1")
